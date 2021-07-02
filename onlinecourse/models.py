d9000d75bcc29c8dfa78875d0a995ad5fc17d9ed
Régis J.-P. Garnier <regis.garnier@wanadoo.fr>
2021-07-02 12:07:08 +0200

    with requested model and migration done

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,5 +1,6 @@
 import sys
 from django.utils.timezone import now
+from django.template.defaultfilters import default
 try:
     from django.db import models
 except Exception:
@@ -16,6 +17,7 @@ class Instructor(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
+    id=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     full_time = models.BooleanField(default=True)
     total_learners = models.IntegerField()
 
@@ -25,6 +27,7 @@ class Instructor(models.Model):
 
 # Learner model
 class Learner(models.Model):
+    id=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -54,6 +57,7 @@ class Learner(models.Model):
 
 # Course model
 class Course(models.Model):
+    id=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     name = models.CharField(null=False, max_length=30, default='online course')
     image = models.ImageField(upload_to='course_images/')
     description = models.CharField(max_length=1000)
@@ -70,6 +74,7 @@ class Course(models.Model):
 
 # Lesson model
 class Lesson(models.Model):
+    id=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     title = models.CharField(max_length=200, default="title")
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -88,47 +93,40 @@ class Enrollment(models.Model):
         (HONOR, 'Honor'),
         (BETA, 'BETA')
     ]
+    id=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    # ternary data link as composite primary key (why an PK id ?)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    #
     date_enrolled = models.DateField(default=now)
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
-
-# <HINT> Create a Question Model with:
-    # Used to persist question content for a course
-    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
-    # Has a grade point for each question
-    # Has question content
-    # Other fields and methods you would like to design
-#class Question(models.Model):
-    # Foreign key to lesson
-    # question text
-    # question grade/mark
-
-    # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
-
-
-#  <HINT> Create a Choice Model with:
-    # Used to persist choice content for a question
-    # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
-    # Choice content
-    # Indicate if this choice of the question is a correct one or not
-    # Other fields and methods you would like to design
-# class Choice(models.Model):
-
-# <HINT> The submission model
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    chocies = models.ManyToManyField(Choice)
-#    Other fields and methods you would like to design
+class Question(models.Model):
+    '''Used to persist question content for a course'''
+    id=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    text = models.CharField(max_length=200)# Has question content
+    grade=models.FloatField(default=1.0)# Has a grade point for each question
+    course= models.ForeignKey(Course, on_delete=models.CASCADE)# Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
+    #lesson = models.ManyToManyField(Lesson, on_delete=models.CASCADE, through='Course') # Foreign key to lesson # ???
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False      
+    
+class Choice(models.Model):
+    ''' Used to persist choice content for a question '''
+    id=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    text = models.CharField(max_length=200) # Choice content
+    is_correct = models.BooleanField(default=False) # Indicate if this choice of the question is a correct one or not
+    question= models.ForeignKey(Question, on_delete=models.CASCADE) # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
+    
+class Submission(models.Model): 
+    ''' Used to link an enrollment to multiples choices''' 
+    id=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    enrollment=models.ForeignKey(Enrollment, on_delete=models.CASCADE) # One enrollment could have multiple submission AND one submission is for one enrollment only
+    choices = models.ManyToManyField(Choice) # One submission could have multiple choices AND One choice could belong to multiple submissions
+    
